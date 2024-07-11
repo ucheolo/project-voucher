f94285a9216cff8c3dc063f87ff006fafefa24b3
ucShin <swc9704@gmail.com>
2024-07-11 23:42:53 +0900

    project init

---
+++ src/main/java/com/example/project_voucher/ProjectVoucherApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_voucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectVoucherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectVoucherApplication.class, args);
+	}
+
+}
