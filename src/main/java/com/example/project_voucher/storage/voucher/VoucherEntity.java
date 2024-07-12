96c3f64bf755851ff97ca47c3271cd679beb586e
ucShin <swc9704@gmail.com>
2024-07-13 01:08:25 +0900

    [기본 구현 1차] dynamic test

--- src/main/java/com/example/project_voucher/storage/voucher/VoucherEntity.java
+++ src/main/java/com/example/project_voucher/storage/voucher/VoucherEntity.java
@@ -48,10 +48,18 @@ public class VoucherEntity extends BaseEntity {
     }
 
     public void disable() {
+        if (!this.status.equals(VoucherStatusType.PUBLISH)) {
+            throw new IllegalStateException("사용 불가 처리할 수 없는 상태의 상품권 입니다.");
+        }
+
         this.status = VoucherStatusType.DISABLE;
     }
 
     public void use() {
+        if (!this.status.equals(VoucherStatusType.PUBLISH)) {
+            throw new IllegalStateException("사용할 수 없는 상태의 상품권 입니다.");
+        }
+
         this.status = VoucherStatusType.USE;
     }
 }
