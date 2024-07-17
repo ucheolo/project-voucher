4e0a2ca780e45c80a3f8dc43082fe1d7ec523092
ucShin <swc9704@gmail.com>
2024-07-17 17:52:13 +0900

    [검증 로직 추가] Validator 수정

--- src/main/java/com/example/project_voucher/domain/service/validator/VoucherDisableValidator.java
+++ src/main/java/com/example/project_voucher/domain/service/validator/VoucherDisableValidator.java
@@ -8,6 +8,11 @@ import org.springframework.stereotype.Component;
 public class VoucherDisableValidator {
 
     public void validate(VoucherEntity voucherEntity, RequestContext requestContext) {
+        상품권_사용_불가_처리_권한이_있는지_확인(voucherEntity, requestContext);
+    }
+
+    public static void 상품권_사용_불가_처리_권한이_있는지_확인(VoucherEntity voucherEntity, RequestContext requestContext) {
+        // 상품권 사용 불가 처리 권한이 있는지 확인
         if (voucherEntity.publishHistory().requesterType() != requestContext.requesterType()
                 || !voucherEntity.publishHistory().requesterId().equals(requestContext.requesterId())) {
             throw new IllegalArgumentException("사용 불가 처리 권한이 없는 상품권 입니다.");
--- src/main/java/com/example/project_voucher/domain/service/validator/VoucherPublishValidator.java
+++ src/main/java/com/example/project_voucher/domain/service/validator/VoucherPublishValidator.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Component;
 public class VoucherPublishValidator {
 
     public void validate(ContractEntity contractEntity) {
+        상품권_발행을_위한_계약_유효기간이_만료되었는지_확인(contractEntity);
+    }
+    public static void 상품권_발행을_위한_계약_유효기간이_만료되었는지_확인(ContractEntity contractEntity) {
+        // 상품권 발행을 위한 계약 유효기간이 만료되었는지 확인
         if (contractEntity.isExpired()) {
             throw new IllegalStateException("유효기간이 지난 계약입니다.");
         }
